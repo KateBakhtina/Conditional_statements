701c8f66da43c8bb7dd457a25c2bb9fb2f486ac3
Kate Bakhtina <almazniegori@gmail.com>
2024-10-08 00:18:25 +0300

    initial_commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Задание 2 — обязательное
+
+        int startCounter = 100;
+        int bonusCounter = 1000;
+
+        int additiveSum = 1100;
+        int resultSum = startCounter + additiveSum;
+
+        if (resultSum > bonusCounter) {
+            int bonus = additiveSum / 100;
+            System.out.println("Итоговый счёт: " + (resultSum + bonus));
+            System.out.println("Количество бонусных рублей: " + bonus);
+        }
+        else {
+            System.out.println("Итоговый счёт: " + resultSum);
+        }
+
+    }
+}
